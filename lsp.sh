c0e47426c4dbcb5228baa621956b825398371a40
Nick Matvyeyev <namatv@zorya.com>
2011-09-01 17:49:43 +0300

    Added uninslellers for *.lsp *.sh[xp] files.

--- lsp.sh
+++ lsp.sh
@@ -1,6 +1,12 @@
 assa=`find . -name "*.lsp"`
-for i in $assa; do  echo "  File $i"; done >lsp.txt
+for i in $assa; do  echo "  File -r $i"; done >lsp.txt
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
   sed -i "s/\//\\\/" lsp.txt
+done
+
+for i in $assa; do  echo "  Delete \$INSTDIR/$i"; done >un.lsp.txt
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+do
+  sed -i "s/\//\\\/" un.lsp.txt
 done
--- sh[xp].sh
+++ sh[xp].sh
@@ -1,8 +1,13 @@
 echo 'Section "sh[xp]"' >sh[xp].txt
-echo '  SetOutPath $INSTDIR' >>sh[xp].txt
 
 assa=`find . -name "*.sh[xp]"`
-for i in $assa; do  echo "  File $i"; done >>sh[xp].txt
+for i in $assa
+do
+  dn=`dirname $i`
+
+  echo "  SetOutPath \$INSTDIR/$dn"
+  echo "  File $i"
+done >>sh[xp].txt
 
 echo 'SectionEnd' >>sh[xp].txt
 
@@ -10,3 +15,14 @@ for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
   sed -i "s/\//\\\/" sh[xp].txt
 done
+
+
+echo 'Section "un.sh[xp]"' >un.sh[xp].txt
+
+for i in $assa; do  echo "  Delete \$INSTDIR/$i"; done >>un.sh[xp].txt
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+do
+  sed -i "s/\//\\\/" un.sh[xp].txt
+done
+
+echo 'SectionEnd' >>un.sh[xp].txt
