8c27db1355e9cae22e01112bb5546a66b218db39
Nick Matvyeyev <mnasoft@gmail.com>
2013-07-07 23:49:46 +0400

    modified:   walk_dir.py

--- walk_dir.py
+++ walk_dir.py
@@ -1,16 +1,39 @@
 import os
+
 import os.path
+
 f_lst=[]
-for root, dirs, files in os.walk('.'):
-  if '.git' in dirs:
-    dirs.remove('.git')
-  for name in files:
-    print(os.path.join(root, name))
-    f_lst.append(os.path.join(root, name))
+
+extension='.lsp'
+
+ext_lst=['.lsp', '.dcl','.prj', '.prv']
+
+def find_files(pth='.'):
+  global f_lst
+  for root, dirs, files in os.walk(pth):
+    if '.git' in dirs:
+      dirs.remove('.git')
+    for name in files:
+      f_lst.append(os.path.join(root, name))
+    
 def str_iter(string):
-  if string[-len(extension):]==extension: 
+  if string[-len(extension):]==extension:
     return True
-  else: 
+  else:
     return False
-extension='.js'
-filter(str_iter, f_lst)
+
+def str_select(ext):
+  global extension
+  extension=ext
+  for s in filter(str_iter, f_lst):
+    print(s)
+
+def select_exts(exts,pth='.'):
+  global f_lst
+  global extension
+  f_lst=[]
+  find_files(pth)
+  for i in exts:
+    extension=i
+    for s in filter(str_iter, f_lst):
+     print(s)
