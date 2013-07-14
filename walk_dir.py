9a52d4c7b2a4ebff018544d17a67125bf2d695e1
Nick Matvyeyev <mnasoft@gmail.com>
2013-07-14 10:10:33 +0400

    modified:   walk_dir.py

--- walk_dir.py
+++ walk_dir.py
@@ -6,13 +6,22 @@ f_lst=[]
 
 extension='.lsp'
 
-ext_lst=['.lsp', '.dcl','.prj', '.prv']
+#ext_lst=['.txt']
+ext_lst=['.EXE', '.INI', '.VLX', '.arx', '.bat', '.bmp', '.cui', '.cuix', '.dat', '.dbx', '.dcl', '.dwg', '.fnt', '.fon', '.glb', '.hdx', '.html', '.ijk', '.jpg', '.js', '.lin', '.log', '.lsp', '.mea', '.mnl', '.nsi', '.php', '.png', '.prj', '.prv', '.rtf', '.sh', '.shp', '.shx', '.slb', '.sld', '.ttf', '.txt', '.dot', '.pdf', '.scr']
 
 def find_files(pth='.'):
   global f_lst
   for root, dirs, files in os.walk(pth):
+    #print('ROOT\n')
+    #print(root)
+    #print('DIRS')
+    #print(dirs)
+    #print("FILES")
+    #print(files)
     if '.git' in dirs:
       dirs.remove('.git')
+    if '__pycache__' in dirs:
+      dirs.remove('__pycache__')
     for name in files:
       f_lst.append(os.path.join(root, name))
     
@@ -37,3 +46,154 @@ def select_exts(exts,pth='.'):
     extension=i
     for s in filter(str_iter, f_lst):
      print(s)
+     
+def create_nsi():
+  f_nsi=open("text2.txt", 'w')
+  f_nsi.write('''; MNAS_acad_utils.nsi
+
+; The name of the installer
+Name "mnas_acad_utils"
+''')
+
+  f_nsi.write('''
+!define VERSION "$UTILS_VERSION"
+''')
+
+  f_nsi.write('''
+LoadLanguageFile "${NSISDIR}\\Contrib\\Language files\\English.nlf"
+LoadLanguageFile "${NSISDIR}\\Contrib\\Language files\\Russian.nlf"
+
+VIProductVersion "${VERSION}"
+VIAddVersionKey /LANG=${LANG_ENGLISH} "ProductName" "Mnas_acad_utils"
+VIAddVersionKey /LANG=${LANG_ENGLISH} "Comments" "Utils for work with AutoCAD© (2007-2011)"
+VIAddVersionKey /LANG=${LANG_ENGLISH} "CompanyName" "Nick Matvyeyev (MNASoft)"
+VIAddVersionKey /LANG=${LANG_ENGLISH} "LegalTrademarks" "http:\\\\www.mnasoft.mksat.net"
+VIAddVersionKey /LANG=${LANG_ENGLISH} "LegalCopyright" "License: GNU GPL v3.0"
+VIAddVersionKey /LANG=${LANG_ENGLISH} "FileDescription" "Утилиты для работы с AutoCAD© (2007-2011)"
+VIAddVersionKey /LANG=${LANG_ENGLISH} "FileVersion" "${VERSION}"
+
+VIAddVersionKey /LANG=${LANG_RUSSIAN} "ProductName" "Mnas_acad_utils"
+VIAddVersionKey /LANG=${LANG_RUSSIAN} "Comments" "Утилиты для работы с AutoCAD© (2013)"
+VIAddVersionKey /LANG=${LANG_RUSSIAN} "CompanyName" "Матвеев Николай Анатольевич (MNASoft)"
+VIAddVersionKey /LANG=${LANG_RUSSIAN} "LegalTrademarks" "http:\\\\www.mnasoft.mksat.net"
+VIAddVersionKey /LANG=${LANG_RUSSIAN} "LegalCopyright" "Распространяется по лицензии GNU GPL v3.0"
+VIAddVersionKey /LANG=${LANG_RUSSIAN} "FileDescription" "Утилиты для работы с AutoCAD© (2013)"
+VIAddVersionKey /LANG=${LANG_RUSSIAN} "FileVersion" "${VERSION}"
+
+
+; The file to write
+OutFile "mnas_acad_utils-${VERSION}.exe"
+
+; The default installation directory
+InstallDir $PROGRAMFILES\\MNASoft\\MNAS_acad_utils
+
+; Registry key to check for directory (so if you install again, it will 
+; overwrite the old one automatically)
+
+InstallDirRegKey HKLM "Software\\MNASoft" "ROOT"
+
+; Request application privileges for Windows Vista
+RequestExecutionLevel admin
+
+LicenseData ./gpl-3.0.txt
+
+;______________________________________________________________________________
+
+; Pages
+Page license
+Page components
+Page directory
+Page instfiles
+
+UninstPage uninstConfirm
+UninstPage instfiles
+
+;______________________________________________________________________________
+
+; The stuff to install
+Section "mnas_acad_utils (required)"
+
+  SectionIn RO
+  
+  ; Set output path to the installation directory.
+  SetOutPath $INSTDIR
+  
+  ; Put file there
+
+  ; Write the installation path into the registry
+  
+  WriteRegStr HKLM SOFTWARE\\MNASoft    "ROOT" "$INSTDIR"
+  WriteRegStr HKLM SOFTWARE\\MNASoft\\16 "ROOT" "$INSTDIR"
+  WriteRegStr HKLM SOFTWARE\\MNASoft\\17 "ROOT" "$INSTDIR"
+  WriteRegStr HKLM SOFTWARE\\MNASoft\\18 "ROOT" "$INSTDIR"
+  WriteRegStr HKLM SOFTWARE\\MNASoft\\19 "ROOT" "$INSTDIR"
+  
+  ; Write the uninstall keys for Windows
+  WriteRegStr HKLM "Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\mnas_acad_utils" "DisplayName" "mnas_acad_utils"
+  WriteRegStr HKLM "Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\mnas_acad_utils" "UninstallString" "$INSTDIR\\uninstall.exe"
+  WriteRegDWORD HKLM "Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\mnas_acad_utils" "NoModify" 1
+  WriteRegDWORD HKLM "Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\mnas_acad_utils" "NoRepair" 1
+  WriteUninstaller "uninstall.exe"
+  
+SectionEnd
+
+; Optional section (can be disabled by the user)
+Section "Start Menu Shortcuts"
+
+  CreateDirectory "$SMPROGRAMS\\mnas_acad_utils"
+  
+  CreateShortCut "$SMPROGRAMS\\mnas_acad_utils\\Uninstall.lnk"               "$INSTDIR\\uninstall.exe"           "" "$INSTDIR\\uninstall.exe" 0
+  CreateShortCut "$SMPROGRAMS\\mnas_acad_utils\\README.html.lnk"              "$INSTDIR\\README.html"              "" "" 0  
+  CreateShortCut "$SMPROGRAMS\\mnas_acad_utils\\Change.log.html.lnk"              "$INSTDIR\\Change.log.html"              "" "" 0  
+  CreateShortCut "$SMPROGRAMS\\mnas_acad_utils\\CopyRight.html.lnk"          "$INSTDIR\\CopyRight.html"          "" "" 0  
+  CreateShortCut "$SMPROGRAMS\\mnas_acad_utils\\Mnasoft_command_list.html.lnk" "$INSTDIR\\acad.help\\mnasoft_command_list.html"          "" "" 0  
+  CreateShortCut "$SMPROGRAMS\\mnas_acad_utils\\CopyRight.rtf.lnk"           "$INSTDIR\\CopyRight.rtf"           "" "" 0  
+  CreateShortCut "$SMPROGRAMS\\mnas_acad_utils\\gpl-3.0.rtf.lnk"             "$INSTDIR\\gpl-3.0.rtf"             "" "" 0  
+  CreateShortCut "$SMPROGRAMS\\mnas_acad_utils\\gpl-3.0-standalone.html.lnk" "$INSTDIR\\gpl-3.0-standalone.html" "" "" 0  
+
+SectionEnd
+
+;_Section_MNASoft_files_Start__________________________________________________''')
+#cat inst.tmp >> $nsi_tmp
+  f_nsi.write('''
+;_Section_MNASoft_files_END____________________________________________________
+
+; Uninstaller
+;______________________________________________________________________________
+''')
+#cat un.inst.tmp >> $nsi_tmp
+  f_nsi.write('''
+;______________________________________________________________________________
+Section "Uninstall"
+  
+  ; Remove registry keys
+  DeleteRegKey HKLM "Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\mnas_acad_utils"
+  
+  DeleteRegKey HKLM SOFTWARE\\MNASoft
+
+  ; Remove files and uninstaller
+  Delete $INSTDIR\\mnas_acad_utils.nsi
+  Delete $INSTDIR\\uninstall.exe
+  
+;_RMDir_Start_______________________________________________________________________
+''')
+#cat rmdir.tmp >> $nsi_tmp
+  f_nsi.write('''
+;_RMDir_End_________________________________________________________________________
+
+  ; Remove shortcuts, if any
+  Delete "$SMPROGRAMS\\mnas_acad_utils\\*.*"
+
+  ; Remove directories used
+  RMDir "$SMPROGRAMS\\mnas_acad_utils"
+  
+  RMDir "$INSTDIR"
+
+SectionEnd
+''')
+  f_nsi.close()
+
+  
+#import walk_dir
+#walk_dir.select_exts(walk_dir.ext_lst)
+#walk_dir.create_nsi()
